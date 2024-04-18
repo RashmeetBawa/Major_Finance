4d55739e667589090d6e65538eae919db44d5df0
Pavani <pavani.code03@gmail.com>
2024-04-18 20:42:00 +0530

    React routing & navigation

--- Finance-Main/src/App.tsx
+++ Finance-Main/src/App.tsx
@@ -1,20 +1,28 @@
-import {createTheme} from "@mui/material/styles"
-import { useMemo } from "react"
-import { themeSettings } from "./theme"
-import { ThemeProvider } from "@emotion/react"
-import { CssBaseline } from "@mui/material"
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import {createTheme} from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter,Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
+import Navbar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings),[])
   return (
-    <>
-      <div className="app">
+    <div className="app">
+      <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          hello world
+            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<div>Dashboard Page</div>}/>
+                <Route path="/predictions" element={<div>Predictions Page</div>}/>
+              </Routes>
+            </Box>
         </ThemeProvider>
-      </div>
-    </>
-  )
+      </BrowserRouter>
+    </div>
+  );
 }
 export default App
