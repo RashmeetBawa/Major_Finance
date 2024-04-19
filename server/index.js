3a19a5992428252934dab1d13d744ccdf4e02945
Pavani <pavani.code03@gmail.com>
2024-04-19 16:12:52 +0530

    p/MongoDb Installation, Setup & Data Upload

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+//
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
@@ -11,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extend: false }));
@@ -20,3 +21,14 @@ app.use(cors());
 console.log("hello");
 
 /* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        
+    })
+    .then(async () => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    })
+    .catch((error) => console.log(`${error} did not connect`));
