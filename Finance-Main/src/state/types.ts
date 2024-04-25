495b2f175344d84092c0562a006a09c5a4fe8247
Pavani <pavani.code03@gmail.com>
2024-04-26 00:43:59 +0530

    p/Products UI & API

--- Finance-Main/src/state/types.ts
+++ Finance-Main/src/state/types.ts
@@ -9,7 +9,6 @@ export interface Month{
     month: string;
     revenue: number;
     expenses: number;
-
     nonOperationalExpenses: number;
     operationalExpenses: number;
 }
@@ -26,10 +25,22 @@ export interface GetKpisResponse {
     _id: string;
     __v: number;
     totalProfit: number;
-
     totalRevenue: number;
     totalExpenses: number;
     expensesByCategory: ExpensesByCategory;
     monthlyData: Array<Month>;
     dailyData: Array<Day>;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface GetProductsResponse {
+    id: string;
+    _id: string;
+    __v: number;
+    price: number;
+    expense: number;
+    transactions: Array<string>;
+    createdAt: string;
+    updatedAt: string;
 }
